f5e48b85b3baecebf31631c49c7181e28a80e908
Yousen Zhang <yzhang11@bnl.gov>
2026-02-12 18:41:11 -0500

    run example

--- run_pgun_sp.sh
+++ run_pgun_sp.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 python convert.py --no_eventid_as_runid build/pgun_muplus_3gev_nt_MuonSteps.csv pgun_muplus_3gev.hdf5
 python convert.py --no_eventid_as_runid build/pgun_positron_3gev_nt_MuonSteps.csv pgun_positron_3gev.hdf5
 
-python dump_to_bee.py pgun_muplus_3gev.hdf5 pgun_muplus_3gev_json
-python dump_to_bee.py pgun_positron_3gev.hdf5 pgun_positron_3gev_json
+python dump_to_bee.py  --compress pgun_muplus_3gev.hdf5 pgun_muplus_3gev_json
+python dump_to_bee.py  --compress pgun_positron_3gev.hdf5 pgun_positron_3gev_json
